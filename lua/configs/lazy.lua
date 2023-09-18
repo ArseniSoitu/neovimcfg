22b133cb46d5b44f8a9ff4a405b9b51e63f8b8c8
ArseniSoitu <seniasoitu@gmail.com>
2023-09-18 14:57:10 +0300

    Added lazy configuration

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -11,4 +11,17 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({})
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+
+  defaults = {
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  checker = { enabled = true }, -- automatically check for plugin updates
+})
