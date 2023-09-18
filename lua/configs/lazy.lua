a5932bd176bd1444020fadd3938e49e4c4f023ee
ArseniSoitu <seniasoitu@gmail.com>
2023-09-18 15:48:51 +0300

    minimal installation for go, includes ray-x plugin

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -11,17 +11,19 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
+require("lazy").setup(
+{
+    {import = "plugins"}, 
+    {import = "plugins.lsp"}
+},
 
-  defaults = {
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+{
+  checker = {
+    enabled = true,
+    notify = false,
   },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  change_detection = {
+    notify = false,
+  },
 })
+
