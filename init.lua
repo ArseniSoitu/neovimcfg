efb42cded10f70d68e6e173197425380300210a2
ArseniSoitu <seniasoitu@gmail.com>
2023-09-30 21:49:41 +0300

    added bufferline, plugin for tabs and buffers

--- init.lua
+++ init.lua
@@ -1,4 +1,18 @@
 require("configs.keymaps")
 require("configs.options")
 require("configs.lazy")
+require("bufferline").setup{
+    options = {
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = function()
+                    return vim.fn.getcwd()
+                end,
+                highlight = "Directory",
+                text_align = "left"
+            }
+        }
+    }
+}
 vim.cmd.colorscheme "catppuccin-mocha"
